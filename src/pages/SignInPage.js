21207c458976faad0758703168983ff0f9138dd9
LiciaPereira <78990316+LiciaPereira@users.noreply.github.com>
2025-04-04 19:54:38 -0400

    updated SignInPage's name

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import "../styles/SignUpPage.css";
+import "../styles/SignInPage.css";
 import { auth } from "../firebase";
 import {
   createUserWithEmailAndPassword,
