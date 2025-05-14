0e13630b18885e4a1a37ef70b3a3c0311613c409
Licia Pereira <78990316+LiciaPereira@users.noreply.github.com>
2025-05-14 14:59:45 -0400

    a few more tweaks for responsiveness improvement, specially in small screens.

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -9,28 +9,36 @@ import PlansPage from "./PlansPage";
 
 function ProfilePage() {
   const user = useSelector(selectUser);
+
   return (
     <div className="profilePage">
       <NavBar />
-      <div className="profilePage__body">
-        <h1>Edit Profile</h1>
+      <div className="profilePage__container">
+        <div className="profilePage__body">
+          <h1>Edit Profile</h1>
 
-        <div className="profilePage__info">
-          <img src={avatar} alt="profile avatar"></img>
-          <div className="profilePage__details">
-            <h2>{user.email}</h2>
-            <div className="profilePage__plans">
-              <h3>Plans (Current Plan: Premium)</h3>
-              <PlansPage />
-              <button
-                onClick={() => {
-                  auth.signOut();
-                  window.location.href = "/";
-                }}
-                className="profilePage__signOut"
-              >
-                Sign Out
-              </button>
+          <div className="profilePage__info">
+            <div className="profilePage__avatar">
+              <img src={avatar} alt="Profile avatar" />
+            </div>
+
+            <div className="profilePage__details">
+              <h2>{user.email}</h2>
+              <div className="profilePage__plans">
+                <h3>Plans</h3>
+                <div className="profilePage__plansContainer">
+                  <PlansPage />
+                </div>
+                <button
+                  onClick={() => {
+                    auth.signOut();
+                    window.location.href = "/";
+                  }}
+                  className="profilePage__signOut"
+                >
+                  Sign Out
+                </button>
+              </div>
             </div>
           </div>
         </div>
